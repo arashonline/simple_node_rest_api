9334cfa1531ac5f254e0be43b572d32a835f4793
arash <arash.rabiee.88@gmail.com>
2020-01-08 00:52:39 +0330

    creating auth route + User model + auth controller

--- app.js
+++ app.js
@@ -2,31 +2,33 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+
 const feedRoutes = require('./routes/feed');
+const authRoutes = require('./routes/auth');
 const multer = require('multer');
 
 const app = express();
 
 // configure file storage
 const fileStorage = multer.diskStorage({
-    destination:(req, file, cb)=> {
-        cb(null,'images')
+    destination: (req, file, cb) => {
+        cb(null, 'images')
     },
-    filename:(req, file, cb)=>{
-        const name = Math.random()+'-'+file.originalname;
-        cb(null,name);
+    filename: (req, file, cb) => {
+        const name = Math.random() + '-' + file.originalname;
+        cb(null, name);
     }
 })
 
 // then we define a filter to look for specific mime types
-const fileFilter = (req, file, cb)=>{
-    if(
+const fileFilter = (req, file, cb) => {
+    if (
         file.mimetype === 'image/png' ||
         file.mimetype === 'image/jpg' ||
-        file.mimetype === 'image/jpeg' 
-    ){
+        file.mimetype === 'image/jpeg'
+    ) {
         cb(null, true)
-    }else{
+    } else {
         cb(null, false)
 
     }
@@ -36,16 +38,16 @@ const MONGODB_URI = 'mongodb://localhost:27017/messages?retryWrites=true';
 
 // app.use(bodyParser.urlencoded()); //x-www-form-urlencoded
 
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 // now we register multer
 app.use(multer({
-    storage:fileStorage,
+    storage: fileStorage,
     fileFilter: fileFilter
 }).single('image')
 );
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
-app.use((req,res,next)=>{
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
@@ -54,19 +56,24 @@ app.use((req,res,next)=>{
 })
 
 app.use('/feed', feedRoutes);
+app.use('/auth', authRoutes);
 
-app.use((error, req,res, next)=>{
+app.use((error, req, res, next) => {
     console.log(error);
     const status = error.statusCode || 500;
     const message = error.message
-    res.status(status).json({message:message});
+    const data = error.data;
+    res.status(status).json({
+        message: message,
+        data: data
+    });
 });
 
-mongoose.connect(MONGODB_URI,{ useUnifiedTopology: true,useNewUrlParser: true })
-.then(result =>{
-    app.listen(8022);
-})
-.catch(err=>{
-    console.log(err)
-})
+mongoose.connect(MONGODB_URI, { useUnifiedTopology: true, useNewUrlParser: true })
+    .then(result => {
+        app.listen(8022);
+    })
+    .catch(err => {
+        console.log(err)
+    })
 
