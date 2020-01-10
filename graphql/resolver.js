dd850e132c458620e2b4a501bba6c8c31fe80ecc
arash <arash.rabiee.88@gmail.com>
2020-01-10 16:52:24 +0330

    Adding mutation

--- graphql/resolver.js
+++ graphql/resolver.js
@@ -1,9 +1,4 @@
 // we need a method for every query or mutation in shcema
 module.exports = {
-    hello(){
-        return {
-            text: "Hellow There",
-            views: 1234
-        }
-    }
+   
 };
--- graphql/schema.js
+++ graphql/schema.js
@@ -2,16 +2,36 @@ const {buildSchema} = require('graphql');
 
 //  the ! mark make the query (in hello for instance) require
 module.exports = buildSchema(`
-    type TestData {
-        text:String!
-        views: Int!
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        imageUrl: String!
+        creator: User!
+        createdAt: String!
+        updatedAt: String!
     }
 
-    type RootQuery {
-        hello: TestData
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        password: String
+        status: String!
+        post: [Post!]!
     }
+
+    input UserInputData {
+        email: String!
+        name: String!
+        password: String!
+    }
+
+    type RootMutation {
+        createUser(userInput: UserInputData): User!
+    }    
 
     schema {
-        query: RootQuery
+        mutation: RootMutation
     }
 `);
