f970b6966a831d4b6e937e03f0dbf59a5ec1d951
arash <arash.rabiee.88@gmail.com>
2020-01-12 02:05:14 +0330

    first real test

--- middleware/is-auth.js
+++ middleware/is-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req, res, next)=>{
     const authHeader = req.get('Authorization');
     if(!authHeader){
-        const error = new error('Not Authenticated.')
+        const error = new Error('Not Authenticated.')
         error.statusCode = 401;
         throw error;
     }
@@ -18,7 +18,7 @@ module.exports = (req, res, next)=>{
     }
 
     if(!decodedToken){
-        const error = new error('Not Authenticated.')
+        const error = new Error('Not Authenticated.')
         error.statusCode = 401;
         throw error;
     }
